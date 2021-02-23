d83073f546ac82eddc1a4866d404fb9086c7db24
Amandeep Singh Sandhu <63533238+AmandeepSingh13@users.noreply.github.com>
2021-02-23 10:13:36 +0530

    Add files via upload

---
+++ Hello_Aman.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello_aman;
+
+/**
+ *
+ * @author Amandeep Singh
+ */
+public class Hello_Aman {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello! \"Amandeep Singh\"");
+    }
+    
+}
